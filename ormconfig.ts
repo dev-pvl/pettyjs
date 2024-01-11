f6974894b259b23225ae9559e48d2993f5fd68d8
psyrotyna <psyrotyna@n-ix.com>
2024-01-11 17:52:52 +0200

    ormconfig fix

--- ormconfig.ts
+++ ormconfig.ts
@@ -28,8 +28,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export default (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: 'petty',
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,20 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsBoolean, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsString()
-  email: string;
-
-  @IsString()
-  password: string;
-
-  @IsString()
-  firstName: string;
-
-  @IsString()
-  lastName: string;
-
-  @IsBoolean()
-  isActive: boolean;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
